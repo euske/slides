2580ac7e928197cac8952171fac53aad554c2155
Yusuke Shinyama <yusuke@shinyama.jp>
2023-10-18 23:08:05 +0900

    Updated.

--- tcpip202310/client.js
+++ tcpip202310/client.js
@@ -1,3 +1,3 @@
-let response = await fetch('http://127.0.0.1:8080/');
-let text = await response.text();
-console.log(text);
+fetch('http://127.0.0.1:8080/').then((response) => {
+  response.text().then((text) => console.log(text));
+});
--- tcpip202310/server.js
+++ tcpip202310/server.js
@@ -1,6 +1,6 @@
 let http = require('http');
 let server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/html'}); 
-  response.end('<h1>Hello world</h1>'); 
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<h1>Hello world</h1>');
 });
 server.listen(8080);
