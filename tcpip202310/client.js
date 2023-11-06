bc80aa1b6d54bd938a8eaadd5de553a35552a1c6
Yusuke Shinyama <yshinyama@vmware.com>
2023-11-06 18:08:12 +0900

    Updated.

--- tcpip202310/client.js
+++ tcpip202310/client.js
@@ -1,3 +1,5 @@
-fetch('http://127.0.0.1:8080/').then((response) => {
-  response.text().then((text) => console.log(text));
-});
+(async () => { 
+  let response = await fetch('http://localhost:8080/');
+  let text = await response.text();
+  console.log(text); 
+})();
--- tcpip202310/server.js
+++ tcpip202310/server.js
@@ -1,5 +1,6 @@
 let http = require('http');
 let server = http.createServer((request, response) => {
+  console.log("connected!");
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<h1>Hello world</h1>');
 });
